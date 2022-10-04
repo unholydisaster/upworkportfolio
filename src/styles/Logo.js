5b64242f08bf90106b3c57edddc73dccc0461cf9
Your Name <unholydisaster>
2022-10-04 12:23:52 +0300

    firstchanges

--- src/styles/Logo.js
+++ src/styles/Logo.js
@@ -4,7 +4,7 @@ import { mq } from "./BurgerStyles"
 export const Logoname=styled.p`
 ${mq[0,1]}{
     position:absolute;
-    top:0px;
+    top:10px;
     left:25%;
     font-size:30px;
     z-index:40;
